ebb9f7f754d85d1026031419f0c50d4f98521cef
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2025-01-30 16:05:38 +0800

    adds action panel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useState } from 'react';
 import './App.css';
+import ActionPanel from './components/ActionPanel';
 
 const App = () => {
+  const [tickers, setTickers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [report, setReport] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
   return (
     <>
       <header>
         <p>Cassidy's Totally Safe Crypto Predictions</p>
       </header>
       <main>
+        {!loading && !report && (
+          <ActionPanel
+            tickers={tickers}
+            setTickers={setTickers}
+            // fetchCryptoData={fetchCryptoData}
+            errorMessage={errorMessage}
+            setErrorMessage={setErrorMessage}
+          />
+        )}
       </main>
       <footer>&copy; Not financial advice, please always do your own research!</footer>
     </>
---
+++ src/components/ActionPanel.js
@@ -0,0 +1,42 @@
+import './styles.css';
+
+const ActionPanel = ({ tickers, setTickers, fetchCryptoData, errorMessage, setErrorMessage }) => {
+  const addTicker = (e) => {
+    e.preventDefault();
+    const input = e.target.elements.ticker.value.trim().toUpperCase();
+
+    if (input.length > 2) {
+      setTickers([...tickers, input]);
+      e.target.elements.ticker.value = '';
+      setErrorMessage('');
+    } else {
+      setErrorMessage('You must add at least one ticker (min 3 letters).');
+    }
+  };
+
+  return (
+    <section className="action-panel">
+      <form onSubmit={addTicker}>
+        <label>
+          Add up to 3 crypto tickers in your portfolio below to get a predictions report 👇
+        </label>
+        <div className="form-input-control">
+          <input type="text" name="ticker" placeholder="BTC" />
+          <button type="submit" className="add-ticker-btn">+</button>
+        </div>
+      </form>
+      {errorMessage && <p className="error">{errorMessage}</p>}
+      <p className="ticker-choice-display">{tickers.join(', ')}</p>
+      <button
+        className="generate-report-btn"
+        onClick={fetchCryptoData}
+        disabled={tickers.length === 0}
+      >
+        Generate Report
+      </button>
+      <p className="tag-line">60% of the time, it works every time.</p>
+    </section>
+  );
+};
+
+export default ActionPanel;
