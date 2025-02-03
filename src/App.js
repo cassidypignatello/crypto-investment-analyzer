016a666f5a8ce730c12f2232b201eae50f66109c
Cassidy Pignatello <cassidy.pignatello@gmail.com>
2025-02-03 14:59:08 +0800

    extracts API logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import { fetchCryptoData } from './utils/api';
 import ActionPanel from './components/ActionPanel';
 import LoadingPanel from './components/LoadingPanel';
 
@@ -9,58 +10,6 @@ const App = () => {
   const [report, setReport] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [cryptoList, setCryptoList] = useState([]);
-
-  const fetchCryptoList = async () => {
-    try {
-      const response = await fetch('https://api.coingecko.com/api/v3/coins/list');
-      if (!response.ok) {
-        throw new Error('Failed to fetch crypto list');
-      }
-
-      return await response.json();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const fetchCryptoData = async () => {
-    setLoading(true);
-
-    try {
-      let cryptoDataList = cryptoList;
-
-      if (cryptoList.length === 0) {
-        cryptoDataList = await fetchCryptoList();
-        setCryptoList(cryptoDataList);
-      }
-
-      const responses = await Promise.all(
-        tickers.map(async (ticker) => {
-          const coin = cryptoDataList.find((coin) => coin.symbol.toUpperCase() === ticker);
-          if (!coin) {
-            throw new Error(`No matching ID found for ${ticker}`);
-          }
-          const url = `https://api.coingecko.com/api/v3/coins/${coin.id}/history?date=30-01-2025`;
-          const options = {
-            method: 'GET',
-            headers: { accept: 'application/json', 'x-cg-demo-api-key': process.env.REACT_APP_COINGECKO_API_KEY },
-          };
-          const response = await fetch(url, options);
-          if (!response.ok) {
-            throw new Error(`Error fetching ${ticker}`);
-          }
-          return await response.json();
-        })
-      );
-
-      console.log(responses)
-      setReport(JSON.stringify(responses, null, 2));
-    } catch (error) {
-      setReport('Error fetching crypto data.');
-    } finally {
-      setLoading(false);
-    }
-  };
 
   return (
     <>
@@ -72,7 +21,7 @@ const App = () => {
           <ActionPanel
             tickers={tickers}
             setTickers={setTickers}
-            fetchCryptoData={fetchCryptoData}
+            fetchCryptoData={() => fetchCryptoData(tickers, cryptoList, setCryptoList, setReport, setLoading)}
             errorMessage={errorMessage}
             setErrorMessage={setErrorMessage}
           />
